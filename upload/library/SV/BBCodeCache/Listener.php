fb4e6ee65255ca975de917ba092d9224f449844c
Xon <tazipper1@hotmail.com>
2016-12-29 13:29:28 +0800

    Fix whitespace

--- upload/library/SV/BBCodeCache/Listener.php
+++ upload/library/SV/BBCodeCache/Listener.php
@@ -5,8 +5,8 @@ class SV_BBCodeCache_Listener
     public static function init_dependencies(XenForo_Dependencies_Abstract $dependencies, array $data)
     {
         if (!class_exists('XenForo_BbCode_TextWrapper', false))
-		{
-			include('TextWrapper.php');
-		}
+        {
+            include('TextWrapper.php');
+        }
     }
 }
